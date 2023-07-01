7ebcbaf41506363bf6f016d7c3d88736c9c65f95
rohit joshi <developerjoshi577@gmail.com>
2023-07-01 23:12:36 +0530

    done

---
+++ eventloop.js
@@ -0,0 +1,5 @@
+console.log('first');
+console.log('second');
+console.log('third');
+
+// completely synchronus code.
---
+++ part2.js
@@ -0,0 +1,11 @@
+console.log('first');
+
+setTimeout(()=>{
+    console.log('second')
+},0)
+
+
+console.log('third')
+
+
+// synchronus + timer + prmise queue excution + ST queue excution
